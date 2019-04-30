119ff971d64097246ad83160f84ecdbeb93699f4
Atul Varma <varmaa@gmail.com>
2019-04-30 07:02:06 -0400

    Monkeypatch NYCDB to recognize custom datasets and then add some (#1)
    * Add monkeypatch_nycdb and marshal evictions 18 dataset.
    
    * Add hpd_head_officers dataset.
    
    * Rename RESIDENTIAL_COMMERCIAL_IND column to EVICTION_TYPE.
    
    * Add eviction_filings_1315.
    
    * Remove extraneous comment.

---
+++ monkeypatch_nycdb/new_dataset_transformations.py
@@ -0,0 +1,13 @@
+from nycdb.dataset_transformations import to_csv
+
+
+def marshal_evictions_18(dataset):
+    return to_csv(dataset.files[0].dest)
+
+
+def hpd_head_officers(dataset):
+    return to_csv(dataset.files[0].dest)
+
+
+def eviction_filings_1315(dataset):
+    return to_csv(dataset.files[0].dest)
---
+++ monkeypatch_nycdb/sql/hpd_head_officers.sql
@@ -0,0 +1,1 @@
+CREATE INDEX hpd_head_officers_bbl_idx on hpd_head_officers (bbl);
